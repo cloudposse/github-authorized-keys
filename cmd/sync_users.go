cfc74503597097ca3ae79eb296415a3372b30e07
Igor Rodionov <goruha@gmail.com>
2016-11-24 11:54:40 +0600

    Rename command sync-users to sync_users (because cobra autogenerate code does not support well commands with '-')

--- cmd/sync_users.go
+++ cmd/sync_users.go
@@ -20,9 +20,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// sync-usersCmd represents the sync-users command
-var sync-usersCmd = &cobra.Command{
-	Use:   "sync-users",
+// sync_usersCmd represents the sync_users command
+var sync_usersCmd = &cobra.Command{
+	Use:   "sync_users",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -32,21 +32,21 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-		fmt.Println("sync-users called")
+		fmt.Println("sync_users called")
 	},
 }
 
 func init() {
-	RootCmd.AddCommand(sync-usersCmd)
+	RootCmd.AddCommand(sync_usersCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// sync-usersCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// sync_usersCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// sync-usersCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// sync_usersCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 }
